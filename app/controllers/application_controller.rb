becbd843646589af191b094f75c3165c774c145a
CCroteau28 <ccroteau28@gmail.com>
2021-06-01 10:25:37 -0600

    commit tree 5672e5cff3d1ec9c9f83bffce1fdf54c00f1240d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,64 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :index
+    @songs = Song.all
+    erb :'songs/index'
   end
 end
+
+#   get '/songs' do
+#     @songs = Song.all
+#     erb :'songs/index'
+#   end
+
+#   get '/songs/new' do
+#     erb :'/songs/new'
+#   end
+
+# post '/songs' do
+#     @songs = Song.create(params[:song])
+#     @songs.genre_ids = params[:genres]
+#     @songs.artist = Artist.find_or_create_by(name: params[:artist][:name])
+#     @songs.save
+#     redirect to :'/songs/#{@song.slug}'
+#   end
+
+# get '/songs/:slug' do
+#     @song = Song.find_by_slug(params[:slug])
+#     erb :'/songs/show'
+#   end
+
+# get '/songs/:slug/edit' do
+#     @song = Song.find_by_slug(params[:slug])
+#     erb :'/songs/edit'
+#   end
+
+# patch '/songs/:slug' do
+#     @song = Song.find_by_slug(params[:slug])
+#     @song.update(params[:song])
+#     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+#     @song.genre_ids = params[:genres]
+#     @song.save
+#     redirect to :'/songs/#{@song.slug}'
+#   end
+
+#   get '/genres' do
+#     @genres = Genre.all
+#     erb '/genres/index'
+#   end
+
+# get '/genres/:slug' do
+#     @genre = Genre.find_by_slug(params[:slug])
+#     erb :'/genres/show'
+#   end
+
+#   get '/artists' do
+#     @artists = Artist.all 
+#     erb :'/artists/index'
+#   end
+
+# get '/artists/:slug' do
+#     @artist = Artist.find_by_slug(params[:slug])
+#     erb :'/artists/show'
+#   end
+# end
